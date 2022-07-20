b96ca57e0711f58d5f18265144a03d5afd7964c6
nacho <nacho.mota@gmail.com>
2022-07-21 00:52:59 +0200

    u ok

--- sources/src/print/ft_print_unsint.c
+++ sources/src/print/ft_print_unsint.c
@@ -17,53 +17,7 @@
 #include "libft.h"
 #include "ft_printf.h"
 
-// static int	ft_numlen(unsigned int nb)
-// {
-// 	int	len;
-
-// 	len = 0;
-// 	while (nb != 0)
-// 	{
-// 		len++;
-// 		nb /= 10;
-// 	}
-// 	return (len);
-// }
-
-// static char *ft_unsing_itoa(unsigned long nb)
-// {
-// 	int	len;
-// 	char	*num;
-// 	len = ft_numlen(nb);
-// 	num = (char *)ft_calloc((len +1), sizeof(char));
-// 	if (!num)
-// 		return (NULL);
-// 	num[len] = '\0';
-// 	while (nb != 0)
-// 	{
-// 		num[len -1] = nb % 10 + '0';
-// 		nb /= 10;
-// 		len--;
-// 	}
-// 	return (num);
-// }
-
-// int	ft_print_unsint(va_list arg, t_print *tp)
-// {
-// 	char *num;
-// 	unsigned long	u;
-// 	u = va_arg(arg, unsigned long);
-// 	num = ft_unsing_itoa(u);
-
-// // printf("dos %s\n", num);
-// 	ft_putstr_fd(num, 1);
-// 	tp->len += ft_strlen(num);
-// 	free(num);
-// 	return (1);
-// }
-//===========================================================================================
-
-static void	u_itoa_hex(unsigned long long nbr, t_print *tp)
+static void	u_itoa_hex(unsigned int nbr, t_print *tp)
 {
 	int	i;
 
@@ -79,23 +33,13 @@ static void	u_itoa_hex(unsigned long long nbr, t_print *tp)
 		ft_putchar_fd(nbr + '0', 1);
 }
 
-
 int	ft_print_unsint(va_list arg, t_print *tp)
 {
-	unsigned long long	nbr;
+	unsigned long 	nbr;
 	
-	nbr = va_arg(arg, unsigned long long);;
-	// printf("itoa: %d\n",u_itoa_hex(nbr));
+	nbr = va_arg(arg, unsigned int );
 	u_itoa_hex(nbr, tp);
 
 	tp->len += 1;
 	return (1);
 }
-
-
-
-	// printf("aki: %lu\n",(unsigned long) u);
-	// unsigned int	x = 4294967295;
-  // printf("UINT_MAX    :   %u\n", (unsigned int) x);
-
-
