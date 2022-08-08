591e0c5e539f6d1bf00df771ab1ba29d28c14a21
nacho <nacho.mota@gmail.com>
2022-08-08 18:29:43 +0200

    str to tp->to_write OK

--- sources/src/ft_read_text.c
+++ sources/src/ft_read_text.c
@@ -17,30 +17,30 @@
 #include "ft_printf.h"
 #include "libft.h"
 
-static void	ft_write_str(const char *c, t_print *tp)
-{
-	if (!tp->error)
-	{	
-		if (write(1, c, tp->widht) == -1)
-		{
-			tp->error = 1;
-			tp->len = -1;
-		}
-	}
-}
+// static void	ft_write_str(const char *c, t_print *tp)
+// {
+// 	if (!tp->error)
+// 	{
+// 		if (write(1, c, tp->widht) == -1)
+// 		{
+// 			tp->error = 1;
+// 			tp->len = -1;
+// 		}
+// 	}
+// }
 
-const	char	*ft_read_text(t_print *tp, const char *format, va_list arg)
+const char *ft_read_text(t_print *tp, const char *format, va_list arg)
 {
-	char	*next;
+	char *next;
 
 	next = ft_strchr(format, '%');
 	if (tp->zero)
 	{
 		tp->zero = 0;
-		while(*format != 'd' ) // poner las demas...
+		while (*format != 'd') // poner las demas...
 		{
 			ft_memcpy_bonus(tp->to_arg, format);
-			format++; 
+			format++;
 		}
 		tp->num_zero = ft_atoi(tp->to_arg);
 		tp->to_arg = "";
@@ -50,11 +50,12 @@ const	char	*ft_read_text(t_print *tp, const char *format, va_list arg)
 		tp->widht = next - format;
 	else
 		tp->widht = ft_strlen(format);
-	ft_write_str(format, tp);
-	
+	tp->to_write = ft_strjoin(tp->to_write, ft_constchar_to_str_bonus(format, tp->widht));
+	// ft_write_str(format, tp);
+
 	while (*format && *format != '%')
-		{
+	{
 		format++;
-		}
+	}
 	return (format);
 }
--- tmp/const_to_str_limit.c
+++ tmp/const_to_str_limit.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_constchar_to_str_bonus.c                        :+:      :+:    :+:   */
+/*   const_to_str_limit.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmota-bu <nmota-bu@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/08 17:17:45 by nmota-bu          #+#    #+#             */
-/*   Updated: 2022/08/08 17:17:47 by nmota-bu         ###   ########.fr       */
+/*   Created: 2022/08/06 00:21:59 by nmota-bu          #+#    #+#             */
+/*   Updated: 2022/08/08 18:07:10 by nmota-bu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 /* ║                 https://github.com/nach131/42Barcelona                 ║ */
 /* ╚════════════════════════════════════════════════════════════════════════╝ */
 
-#include "ft_printf.h"
-#include "libft.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "../sources/libft/inc/libft.h"
+typedef struct s_print
+{
+    char *to_write;
+    char *to_arg;
+    char *total;
+} t_print;
 
-char *ft_constchar_to_str_bonus(const char *s, int n)
+char *constchar_to_str(const char *s, int n)
 {
     if (!s)
         return NULL;
@@ -26,8 +36,8 @@ char *ft_constchar_to_str_bonus(const char *s, int n)
     res = (char *)malloc(n + 1);
     if (!res)
     {
-       // marcar algun error
-			return NULL;
+        fprintf(stderr, "Memory Allocation Failed! Exiting...\n");
+        exit(EXIT_FAILURE);
     }
     else
     {
@@ -40,4 +50,22 @@ char *ft_constchar_to_str_bonus(const char *s, int n)
         res[i] = '\0';
         return res;
     }
+}
+
+int main(void)
+{
+    t_print tp;
+    char *res;
+    const char *esto = "42 Barcelona";
+    
+    tp.to_arg = "arg";
+    tp.to_write = "write";
+    res = constchar_to_str(esto, 2);
+
+//     // tp.total = ft_strjoin(tp.to_arg, res);
+//     tp.total = ft_strjoin(tp.to_arg, constchar_to_str(esto));
+//     tp.total = ft_strjoin(tp.total, tp.to_write);
+
+    // printf("%s", tp.total);
+    printf("%s", res);
 }
